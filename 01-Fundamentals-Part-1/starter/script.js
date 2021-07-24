73258687b91d1046e07f6d9570dd3481369337be
chagy <chagy_x_@hotmail.com>
2021-07-24 11:15:31 +0700

    05 complete

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,22 @@
+let js = "amazing";
+if(js === 'amazing') alert("Javascript is FUN!");
+
+console.log(40 + 8 + 23 - 10);
+
+let firstName = "Chagy";
+
+console.log(firstName);
+
+let jonas_mati = "JM";
+let $function = 27;
+
+let person = "่jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
